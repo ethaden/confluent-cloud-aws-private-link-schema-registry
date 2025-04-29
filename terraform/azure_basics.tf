462b8cc05378570971e61126aaceff44b7e1c446
Eike Thaden <ethaden@confluent.io>
2025-04-29 17:15:26 +0000

    Added Azure VM

--- terraform/azure_basics.tf
+++ terraform/azure_basics.tf
@@ -42,14 +42,14 @@ resource "azurerm_linux_virtual_machine" "azure" {
   name                = "${var.resource_prefix}-vm"
   resource_group_name = azurerm_resource_group.azure.name
   location            = azurerm_resource_group.azure.location
-  size                = "Standard_F2"
+  size                = "Standard_B2s"
   admin_username      = "adminuser"
   network_interface_ids = [
     azurerm_network_interface.azure.id,
   ]
 
   admin_ssh_key {
-    username   = "${var.resource_prefix}-adminuser"
+    username   = "adminuser"
     public_key = var.ssh_key_public
   }
 
@@ -65,3 +65,65 @@ resource "azurerm_linux_virtual_machine" "azure" {
     caching              = "ReadWrite"
   }
 }
+
+# resource "confluent_private_link_attachment" "cc_env_azure" {
+#   cloud        = "AZURE"
+#   region       = var.azure_region
+#   display_name = "${var.resource_prefix}-platt"
+#   environment {
+#     id = confluent_environment.cc_env.id
+#   }
+# }
+
+# resource "azurerm_private_dns_zone" "cc_env" {
+#   resource_group_name = data.azurerm_resource_group.azure.name
+
+#   name = confluent_private_link_attachment.cc_env_azure.dns_domain
+# }
+
+# resource "azurerm_private_endpoint" "cc_env_azure" {
+#   name                = "${var.resource_prefix}-confluent-1"
+#   location            = var.azure_region
+#   resource_group_name = data.azurerm_resource_group.azure.name
+
+#   subnet_id = azurerm_subnet.subnet[1].id
+
+#   private_service_connection {
+#     name                              = "${var.resource_prefix}-confluent-1"
+#     is_manual_connection              = true
+#     private_connection_resource_alias = confluent_private_link_attachment.cc_env_azure.azure[0].private_link_service_alias
+#     request_message                   = "PL"
+#   }
+# }
+
+# resource "confluent_private_link_attachment_connection" "cc_env_azure" {
+#   display_name = "staging-azure-plattc"
+#   environment {
+#     id = confluent_environment.cc_env.id
+#   }
+#   azure {
+#     private_endpoint_resource_id = azure_privatelink.vpc_endpoint_id
+#   }
+
+#   private_link_attachment {
+#     id = confluent_private_link_attachment.cc_env_azure.id
+#   }
+# }
+
+
+# resource "azurerm_private_dns_zone_virtual_network_link" "cc_env_azure" {
+#   name                  = azurerm_virtual_network.azure.name
+#   resource_group_name   = azurerm_resource_group.azure.name
+#   private_dns_zone_name = azurerm_private_dns_zone.cc_env_azure.name
+#   virtual_network_id    = azurerm_virtual_network.azure.id
+# }
+
+# resource "azurerm_private_dns_a_record" "cc_env_azure" {
+#   name                = "*"
+#   zone_name           = azurerm_private_dns_zone.cc_env_azure.name
+#   resource_group_name = azurerm_resource_group.azure.name
+#   ttl                 = 60
+#   records = [
+#     azurerm_private_endpoint.cc_env_azure.private_service_connection[0].private_ip_address
+#   ]
+# }
